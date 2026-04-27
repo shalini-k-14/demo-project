c28047c60de8110a025af327e505ca27d90c77e3
Thanu-1011 <thanushree1629@gmail.com>
2026-04-27 16:22:55 +0530

    fix syntax errors in App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,21 +1,20 @@
-package com.devops
+package com.devops;
 import java.util.Scanner;
-public class App{
-	public static void main(String[] args){
-		Scanner sc = new.Scanner(System.in)
-		int a = sc.nextInt();
-		int b =sc.nextInt();
-		char op =sc.next().CharAt(0);
-		if(op=='+')
-			System.out.println(a+b);
-		else if(op=='-')
-			System.out.println(a-b);
-		else if(op=='*')
-			System.out.println(a*b);
-		else if(op=='/')
-			System.out.println(a/b);
-		else
-			System.out.println(Invalid");
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char op = sc.next().charAt(0);
+        if (op == '+')
+            System.out.println(a + b);
+        else if (op == '-')
+            System.out.println(a - b);
+        else if (op == '*')
+            System.out.println(a * b);
+        else if (op == '/')
+            System.out.println(a / b);
+        else
+            System.out.println("Invalid");
+    }
 }
-}
-	
